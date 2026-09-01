e14d1a77c7a66e4c0bc38dfcf4958cf5c80c4ccd
Priyanka0619 <ramesh0989v@gmail.com>
2026-09-01 19:23:38 +0530

    Add files via upload

---
+++ tsk2.js
@@ -0,0 +1,190 @@
+/*var studentName="jeni";
+let  studentAge="27";
+const collegeName="Einstein college";
+console.log(studentName);
+console.log(studentAge);
+console.log(collegeName);
+studentName="priya";
+console.log(studentName);
+studentAge="29";
+console.log(studentAge);
+collegeName="FX";
+console.log(collegeName);
+
+var studentName="priyanka";
+console.log(studentName);
+let studentAge="30";
+console.log(studentAge); */
+
+
+/*var name=prompt("What is your name");
+var age=prompt("What is your age");
+var city=prompt("Name of your city");*/
+
+/* var name=prompt("What is your name");
+alert("welcome jeni"); */
+ 
+/* let birthYear=1997
+let currentYear=2026;
+let age=currentYear-birthYear;
+console.log(age); */
+
+/*let a="Hello";
+let b="100";
+let c="25.5";
+let d="true";
+let e="false";
+let f="undefined";
+let g="null";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g); */
+
+/*let student= {
+    name:"jeni",
+    age:20,
+    city:"tenkasi",
+    qualification:"UG",
+    isStudent:true
+    };
+     console.log(student);
+     console.log(student.name);
+      console.log(student.age);
+       console.log(student.city);
+       console.log(student.qualification);
+        console.log(student.isStudent); */
+
+/*
+const fruits=["Apple","Mango","Orange","Banana","Grapes","Papaya"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[fruits.length-1]);
+console.log(fruits.length); */
+
+/*let a=20;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);*/
+
+/*const shirt=999;
+const pant=1499;
+const shoes=1999;
+const total=shirt+pant+shoes;
+console.log(total);
+*/
+
+/*const tamil=80;
+const english=75;
+const maths=90;
+const total=tamil+english+maths;
+const average=total/3;
+console.log(total);
+console.log(average);
+*/
+
+/*let a=10;
+let b=a++;
+console.log(a);
+console.log(b); */
+
+/* let a=10;
+let b=++a;
+console.log(a);
+console.log(b); */
+
+/* let a=20;
+let b=a--;
+console.log(a);
+console.log(b); */
+
+/*let a=20;
+let b=--a;
+console.log(a);
+console.log(b); */
+
+/*let a=5;
+let b=a++;
+let c=++a;
+let d=b--;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+*/
+
+/*let num=10;
+num += 5;
+console.log(num);*/
+
+/*let num=10;
+num -= 5;
+console.log(num);*/
+
+/*let num=10;
+num -= 5;
+console.log(num);*/
+
+/*let num=10;
+num *= 5;
+console.log(num);*/
+
+/*let num=10;
+num /= 5;
+console.log(num);*/
+
+/*let num=10;
+num %= 5;
+console.log(num);*/
+
+/*let num=10;
+num **= 5;
+console.log(num);*/
+
+/*let Name="Jeni";
+let Age=21;
+let City="tenkasi";
+let College="xyz University";
+
+let subjects=["js","HTML","CSS","Python","Database"];
+
+let student={
+    name:"Name",
+    age:Age,
+    city:City,
+    subjects:subjects,
+    isStudent:true
+
+};
+console.log(student.name);
+console.log(student.age);
+console.log(student.city);
+console.log(student.subjects[0]);
+console.log(student.subjects[student.subjects.length-1]);
+console.log(student.subjects.length);
+console.log(student);
+*/
+
+let num1=Number(prompt("enter first number"));
+let num2=Number(prompt("enter second number"));
+
+let addition=num1+num2;
+let subtraction=num1-num2;
+let multiplication=num1*num2;
+let division=num1/num2;
+let modulus=num1%num2;
+let power=num1**num2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+console.log(power);
