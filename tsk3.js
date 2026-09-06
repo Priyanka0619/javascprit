7477b69a49a04eb6b73a939a2714183c4493e01c
Priyanka0619 <ramesh0989v@gmail.com>
2026-09-06 14:57:14 +0530

    Add files via upload

---
+++ tsk3.js
@@ -0,0 +1,383 @@
+/*var name = "Jenifer";
+let age = 25;
+const city = "Chennai";
+console.log(name);
+console.log(age);
+console.log(city);
+
+name = "Priya";
+console.log(name);
+
+age = 26;
+console.log(age);
+
+city = "Coimbatore";
+console.log(city);
+
+var name = "Kumar";
+console.log(name);
+
+let age = 30;
+console.log(age);
+
+const city= "Madurai";
+console.log(city);*/
+
+
+
+
+/*console.log("Hello JavaScript");
+
+alert("Welcome to JavaScript");
+
+
+confirm("Are you learning JavaScript?");
+
+
+let userName = prompt("Enter your name:");
+console.log(userName);
+
+
+document.writeln("Hello, Welcome");*/
+
+/*let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let city = prompt("Enter your city:");
+let qualification = prompt("Enter your qualification:");
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+console.log("Qualification:", qualification);*/
+
+/*let a = "JavaScript";
+let b = 100;
+let c = 99.5;
+let d = true;
+let e = false;
+let f = undefined;
+let g = null;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g); */
+
+/*let students = ["Arun", "Priya", "Kumar", "Divya", "Rahul"];
+
+console.log("First student:", students[0]);
+
+console.log("Second student:", students[1]);
+
+console.log("Last student:", students[4]);
+
+console.log("Total students:", students.length);*/
+
+/*var employee = {
+    name: "Jenifer",
+    age: 25,
+    role: "Full Stack Developer",
+    skills: ["HTML", "CSS", "JavaScript"],
+    isWorking: true,
+    qualification: ["BE CSE", "BE ECE"]
+};
+
+console.log("Employee Name:", employee.name);
+console.log("Age:", employee.age);
+console.log("Role:", employee.role);
+console.log("First Skill:", employee.skills[0]);
+console.log("Last Qualification:", employee.qualification[1]);
+console.log("Working Status:", employee.isWorking);*/
+
+/*let a = 20;
+let b = 5;
+console.log("Addition:", a+b);
+console.log("Subtraction:", a-b);
+console.log("Multiplication:", a*b);
+console.log("Division:", a/b);
+console.log("Modulus:", a%b);
+console.log("Exponentiation:", a**b);*/
+
+/*let shirt = 999;
+let pant = 1499;
+let shoes = 1999;
+let bag = 799;
+
+let total = shirt+pant+shoes+bag;
+console.log("Total price:", total);*/
+
+/*let a = 10;
+let b = a++;
+
+console.log(a);
+console.log(b); */
+
+/*let a = 10;
+let b = ++a;
+
+console.log(a);
+console.log(b); */
+
+/*let a = 10;
+let b = a--;
+
+console.log(a);
+console.log(b);*/
+
+/*let a = 10;
+let b = --a;
+
+console.log(a);
+console.log(b);*/
+
+/*let num = 10;
+num += 5;
+console.log(num);
+
+num = 10;
+num -= 3;
+console.log(num); 
+
+num = 10;
+num *= 2;
+console.log(num); 
+
+num = 10;
+num = 4;
+console.log(num); 
+
+num = 10;
+num %= 3;
+console.log(num); 
+
+num = 10;
+num **= 2;
+console.log(num);*/
+
+/*console.log(10 > 5);       
+console.log(10 < 5);      
+console.log(10 >= 10);     
+console.log(10 <= 9);      
+
+console.log(5 == "5");     
+console.log(5 === "5");    
+
+console.log(10 != "10");  
+console.log(10 !== "10");  */
+
+/*console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);*/
+
+/*console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);*/
+
+/*console.log(!true);
+console.log(!false);
+console.log(!(5 > 10));
+console.log(!(10 > 5));*/
+
+/*console.log(5 == "5" && !(5 === 5) || 6 > 7);
+console.log(10 > 5 && 8 < 12 || 4 === "4");
+console.log(7 === 7 && 10 != "10" || 5 >= 5);
+console.log(15 < 10 || 20 > 15 && 5 == "5");*/
+
+/*var age = 20;
+var result = age>= 18? "Eligible to vote":"Not eligible";
+console.log(result);*/
+
+/*var password = true;
+var result = password?"Login successful" : "Wrong password";
+console.log(result);*/
+
+/*var password = false;
+var result = password?"Login successful" : "Wrong password";
+console.log(result);*/
+/*
+let name = "Naveen";
+let age = 25;
+let city = "Trichy";
+console.log(" My name is " + name + ". I am " + age + " years old. I live in " + city + ".");*/
+
+/*let name = "Naveen";
+let age = 25;
+let city = "Trichy";
+console.log(`My name is ${name}. I am ${age} years old. I live in ${city}.`);*/
+
+/*let a = String(100);
+let b = String(true);
+let c = String(undefined);
+let d = String(null);
+let e = String([1, 2]);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+console.log(d, typeof d);
+console.log(e, typeof e);*/
+
+/*console.log(Number());
+console.log(Number(""));
+console.log(Number("123"));
+console.log(Number("a1"));
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number(null));*/
+
+/*console.log(Boolean());
+console.log(Boolean(""));
+console.log(Boolean("hello"));
+console.log(Boolean(123));
+console.log(Boolean(true));
+console.log(Boolean(false));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean([]));
+console.log(Boolean({}));*/
+
+/*let age =(prompt("Enter your age:"));
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("You can't vote");
+} */
+
+/*let number =(prompt("Enter a number:"));
+if (number>0) {
+    console.log("Positive");
+} else if (number<0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+} */
+
+/*let marks =(prompt("Enter your marks:"));
+if (marks >= 90 && marks <= 100) {
+    console.log("A Grade");
+} else if (marks >= 80) {
+    console.log("B Grade");
+} else if (marks >= 70) {
+    console.log("C Grade");
+} else if (marks >= 60) {
+    console.log("D Grade");
+} else {
+    console.log("Fail");
+} */
+
+/*let age =prompt("Enter your age:");
+let height =prompt("Enter your height:");
+let weight =prompt("Enter your weight:");
+if (age >= 18) {
+
+    if (height >= 160) {
+
+        if (weight >= 60) {
+            console.log("Congratulations! You are selected");
+        } else {
+            console.log("Not selected: Weight must be 60 or above");
+        }
+} 
+else {
+        console.log("Not selected: Height must be 160 or above");
+    }
+} else {
+    console.log("Not selected: Age must be 18 or above");
+} */
+
+    
+/*let color = prompt("Enter traffic light color:");
+
+switch (color) {
+    case "red":
+        console.log("Stop");
+        break;
+
+    case "yellow":
+        console.log("Ready");
+        break;
+
+    case "green":
+        console.log("Go");
+        break;
+
+    default:
+        console.log("Invalid color");
+} */
+
+/*var day = 1;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+     case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+} */
+
+
+/*let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let city = prompt("Enter your city:");
+
+let tamil = prompt("Enter Tamil marks:");
+let english = prompt("Enter English marks:");
+let maths = prompt("Enter Maths marks:");
+
+let total = tamil + english + maths;
+let average = total / 3;
+
+let grade;
+
+if (average >= 90) {
+    grade = "A";
+} else if (average >= 80) {
+    grade = "B";
+} else if (average >= 70) {
+    grade = "C";
+} else if (average >= 60) {
+    grade = "D";
+} else {
+    grade = "Fail";
+}
+
+let voting;
+
+if (age >= 18) {
+    voting = "You can vote";
+} else {
+    voting = "You can't vote";
+}
+
+console.log(`
+Name: ${name}
+Age: ${age}
+City: ${city}
+Total: ${total}
+Average: ${average.toFixed(2)}
+Grade: ${grade}
+Voting: ${voting}
+`);*/
